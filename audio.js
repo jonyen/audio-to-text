c732173a2feacd152ff06ab3a90ab523dc1d8ea6
jonyen <jonyen@gmail.com>
2015-07-06 16:49:33 -0700

    got pocketsphinx.js compiling

--- audio.js
+++ audio.js
@@ -1,21 +1,26 @@
-// Create a new instance of node-core-audio 
+var PocketSphinx = require('./pocketsphinx.js/pocketsphinx.js');
+
+var recognizer = new PocketSphinx.Recognizer();
+
+
+// Create a new instance of node-core-audio
 var coreAudio = require("node-core-audio");
- 
-// Create a new audio engine 
+
+// Create a new audio engine
 var engine = coreAudio.createNewAudioEngine();
- 
-// Add an audio processing callback 
-// This function accepts an input buffer coming from the sound card, 
-// and returns an ourput buffer to be sent to your speakers. 
-// 
-// Note: This function must return an output buffer 
+
+// Add an audio processing callback
+// This function accepts an input buffer coming from the sound card,
+// and returns an ourput buffer to be sent to your speakers.
+//
+// Note: This function must return an output buffer
 function processAudio( inputBuffer ) {
     console.log( "%d channels", inputBuffer.length );
     console.log( "Channel 0 has %d samples", inputBuffer[0].length );
- 
+
     return inputBuffer;
 }
- 
+
 engine.addAudioCallback( processAudio );
 
 
--- test.js
+++ test.js
@@ -1,13 +1,35 @@
-var PocketSphinx = require('./pocketsphinx.js');
+var PocketSphinx = require('./pocketsphinx.js/pocketsphinx.js');
 
 var recognizer = new PocketSphinx.Recognizer();
 
 //var ps = new PocketSphinx();
 
-recognizer.on('utterance', function(hyp, utt, score) {
+/*recognizer.on('utterance', function(hyp, utt, score) {
     console.log( 'Guessed phrase: ' + hyp);
     console.log( 'Confidence score: ' + score);
     console.log( 'Unique utterance id: ' + utt);
 });
 
-recognizer.write(myMicrophoneData);
+recognizer.write(myMicrophoneData); */
+
+/*
+var ps = new PocketSphinx({
+    hmm: '/file/path',
+    lm: '/file/path',
+    dict: '/file/path',
+    samprate: 16000,
+    nfft: 512
+}, function(err, hypothesis, score) {
+
+});
+
+ps.addKeyphraseSearch("keyphrase_name", "keyphrase");
+ps.addKeywordSearch("keyword_name", "/file/path");
+ps.addGrammarSearch("grammar_name", "/file/path");
+ps.addNgramSearch("ngram_name", "/file/path");
+
+ps.search = "keyphrase_name";
+
+ps.start();
+ps.write(data);
+ps.stop(); */
